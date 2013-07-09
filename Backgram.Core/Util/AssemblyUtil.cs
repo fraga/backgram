cf687ab888d35db9f75bddb2f44ce1b16a5ef7f8
Rodrigo Fraga <rodrigo.fraga@gmail.com>
2013-07-08 23:01:18 -0400

    adding assemblyVersionUtil and it's tests

---
+++ Backgram.Core/Util/AssemblyUtil.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Backgram.Core.Util
+{
+    public class AssemblyUtil
+    {
+        public static string GetVersion()
+        {
+            return Assembly.GetExecutingAssembly().GetName().Version.ToString();
+        }
+    }
+}
---
+++ Backgram.Tests/Core/Util/AssemblyUtilTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Backgram.Core.Util;
+using System.Text.RegularExpressions;
+
+namespace Backgram.Tests.Core.Util
+{
+    [TestClass]
+    public class AssemblyUtilTest
+    {
+        [TestMethod]
+        public void GetVersionTest()
+        {
+            Assert.IsFalse(String.IsNullOrEmpty(AssemblyUtil.GetVersion()));
+        }
+
+        [TestMethod]
+        public void GetVersionHasVersionStructureTest()
+        {
+            Assert.IsTrue(Regex.IsMatch(AssemblyUtil.GetVersion(), @"[0-9][.][0-9][.][0-9?]+[.][0-9]+"));
+        }
+    }
+}
