2b685bdb994f346a2d419aa8b4c4a99a0cb332a6
Henrique  Adonai <henriqueadonai@gmail.com>
2013-07-09 21:34:19 -0300

    Add contact form to test my git and commit

---
+++ Backgram/Models/Contact.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Backgram.Models
+{
+    public class Contact
+    {
+
+        public int IDContact { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Subjetct { get; set; }
+    }
+}
