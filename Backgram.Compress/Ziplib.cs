465cc62b47323b72450b909a2ca53aee7b168426
Rodrigo Fraga <rodrigo.fraga@gmail.com>
2013-07-10 23:55:21 -0400

    implements ziplib default constructor and compress method

--- Backgram.Compress/Ziplib.cs
+++ Backgram.Compress/Ziplib.cs
@@ -8,5 +8,20 @@ namespace Backgram.Compress
 {
     public class Ziplib
     {
+        private string _directory;
+
+        public Ziplib()
+        {
+        }
+
+        public Ziplib(string dir)
+        {
+            this._directory = dir;
+        }
+
+        public void Compress()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
