5defbacf28041c81810844df832fa98262bba305
Rodrigo Fraga <rodrigo.fraga@gmail.com>
2013-07-10 22:20:19 -0400

    Placed type Ziplib and initial constructor test

---
+++ Backgram.Tests/Compress/ZiplibTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Backgram.Compress;
+
+namespace Backgram.Tests.Compress
+{
+    [TestClass]
+    public class ZiplibTest
+    {
+        [TestMethod]
+        public void TestConstructorNotNull()
+        {
+            Ziplib zipLib = new Ziplib();
+            Assert.IsNotNull(zipLib);
+        }
+    }
+}
