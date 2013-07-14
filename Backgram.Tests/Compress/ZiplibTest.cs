653d7d15f3a63b82e09e4177184f9a7ec0a59505
Rodrigo Fraga <rodrigo.fraga@gmail.com>
2013-07-14 12:11:14 -0400

    correcting TestContext variable, as it was not being set by the framework (null reference)

--- Backgram.Tests/Compress/ZiplibTest.cs
+++ Backgram.Tests/Compress/ZiplibTest.cs
@@ -2,13 +2,29 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Backgram.Compress;
 using System.IO;
+using System.Text;
 
 namespace Backgram.Tests.Compress
 {
     [TestClass]
     public class ZiplibTest
     {
-        public TestContext Context { get; set; }
+        private TestContext testContextInstance;
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
 
         [TestMethod]
         public void TestConstructorNotNull()
