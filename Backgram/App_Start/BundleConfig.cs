6e97dab892f0a90bca0729f3ba1c091e101a6560
Felipe Seixas <seixasfelipe@gmail.com>
2013-07-17 08:36:09 -0400

    DRY bundle js and css paths

--- Backgram/App_Start/BundleConfig.cs
+++ Backgram/App_Start/BundleConfig.cs
@@ -8,15 +8,18 @@ namespace Backgram
 {
     public class BundleConfig
     {
+        public const string BundlesJs   = "~/bundles/js";
+        public const string BundlesCss  = "~/bundles/css";
+
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/js")
+            bundles.Add(new ScriptBundle(BundleConfig.BundlesJs)
                 .Include("~/Scripts/jquery-{version}.js")
                 .Include("~/Scripts/backgram.js")
                 .Include("~/Scripts/backgram_tools.js")
                 .Include("~/Scripts/backgram_instagram.js"));
 
-            bundles.Add(new StyleBundle("~/bundles/css")
+            bundles.Add(new StyleBundle(BundleConfig.BundlesCss)
                 .Include("~/Content/css/bootstrap.css")
                 .Include("~/Content/css/bootstrap-responsive.css")
                 .Include("~/Content/css/backgram.css"));
