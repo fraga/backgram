4a0870f18cfb2d32e2e9e9e6c4be59e2adc8dee3
Rodrigo Fraga <rodrigo.fraga@gmail.com>
2013-07-17 06:48:40 -0400

    Implementing clientID, passing client side #14

---
+++ Backgram.Core/Util/InstagramUtil.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Backgram.Core.Util
+{
+    public class InstagramUtil
+    {
+        public static string ClientId()
+        {
+            return "fe65cbc6edb44be8be3e9e0e9f4eca64";
+        }
+    }
+}
