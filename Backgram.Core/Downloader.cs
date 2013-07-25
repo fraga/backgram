1ad4e650e325aced6e35b67598a58f933c17de33
Rodrigo Fraga <rodrigo.fraga@gmail.com>
2013-07-25 01:11:08 -0400

    first downloader iteraction, one test ignored

---
+++ Backgram.Core/Downloader.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Backgram.Core
+{
+    public class Downloader
+    {
+        public List<FileInfo> DownloadFiles(List<Uri> uriToDownload, string destinationDirPath)
+        {
+            List<FileInfo> downloadedFiles = new List<FileInfo>();
+
+            if (uriToDownload == null || string.IsNullOrEmpty(destinationDirPath))
+                return downloadedFiles;
+
+            Parallel.ForEach(uriToDownload, async u =>
+                {
+                    await DownloadFileAsync(
+                        u, Path.Combine(destinationDirPath, Guid.NewGuid().ToString())).ContinueWith(
+                            async f => downloadedFiles.Add(await f));
+
+                }
+                );
+
+            return downloadedFiles;
+        }
+
+        public async Task<FileInfo> DownloadFileAsync(Uri uri, string fullFilePath)
+        {
+            if (uri == null || string.IsNullOrEmpty(fullFilePath))
+                throw new ArgumentNullException("URI and Destination Path should not be null");
+
+            using (FileStream newFile = new FileStream(fullFilePath, FileMode.CreateNew))
+            {
+                using (HttpClient client = new HttpClient())
+                {
+                    var httpStream = await client.GetStreamAsync(uri);
+
+                    await httpStream.CopyToAsync(newFile);
+                }
+
+                newFile.Flush();
+            }
+
+            return new FileInfo(fullFilePath);
+        }
+    }
+}
---
+++ Backgram.Tests/Core/DownloaderTest.cs
@@ -0,0 +1,101 @@
+﻿using Backgram.Core;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Backgram.Tests.Core
+{
+    [TestClass]
+    public class DownloaderTest
+    {
+        private TestContext testContextInstance;
+        private string testDir;
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        [TestInitialize]
+        public void Setup()
+        {
+            testDir = Path.Combine(TestContext.TestRunDirectory, "testDownloadedFiles");
+            Directory.CreateDirectory(testDir);
+        }
+
+        [TestMethod]
+        public void ConstructorTestNotNull()
+        {
+            Downloader downloader = new Downloader();
+            Assert.IsNotNull(downloader);
+        }
+        
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public async Task DownloadFileAsyncTestUriArgumentNullException()
+        {
+            Downloader downloader = new Downloader();
+            var file = await downloader.DownloadFileAsync(null, Path.Combine(testDir, "anotherTest.txt"));
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public async Task DownloadFileAsyncTestFileArgumentNullException()
+        {
+            Downloader downloader = new Downloader();
+            var file = await downloader.DownloadFileAsync(new Uri("http://www.google.com/file.gif"), "");
+        }
+
+        [TestMethod]
+        public async Task DownloadFileAsyncShouldDownloadFile()
+        {
+            Downloader downloader = new Downloader();
+            var file = await downloader.DownloadFileAsync(new Uri("http://humanstxt.org/humans.txt"), Path.Combine(testDir, "5mb.zip")); //TODO: change to  file://
+
+            Assert.IsTrue(file != null && file.Exists);
+        }
+
+        [Ignore]
+        public void DownloadFilesShouldDownloadAllFiles()
+        {
+            Downloader downloader = new Downloader();
+            var localDir = Directory.CreateDirectory(Path.Combine(testDir, "testDownloadFilesToDir"));
+
+            List<Uri> uriList = new List<Uri>
+            {
+                new Uri("http://humanstxt.org/humans.txt"),
+                new Uri("http://www.google.com/humans.txt"),
+                new Uri("https://github.com/humans.txt")
+            };
+
+            List<FileInfo> resultList = null;
+
+            try
+            {
+                resultList = downloader.DownloadFiles(uriList, localDir.FullName); //TODO: change to file://
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine(e.Message);
+            }
+
+            Assert.IsNotNull(resultList);
+            Assert.IsTrue(resultList.Count == 3);
+        }
+    }
+}
