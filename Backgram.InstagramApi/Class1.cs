fbd4d40988e1df91e87f5923380b732891ee3a2b
Rodrigo Fraga <rodrigo.fraga@gmail.com>
2013-07-27 14:48:34 -0400

    add new project instagramAPI

---
+++ Backgram.InstagramApi/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Backgram.InstagramApi
+{
+    [Export
+    public class InstagramAPI_v1
+    {
+    }
+}
