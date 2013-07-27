4be32d515ba6e4976bc2b13332d19d7a16189359
Rodrigo Fraga <rodrigo.fraga@gmail.com>
2013-07-27 14:47:43 -0400

    instagram.cs on place

---
+++ Backgram.Core/Instagram.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Backgram.Core
+{
+    public interface Instagram
+    {
+        public string ClientId { get; set; }
+        public string ClientSecret { get; set; }
+        public string RedirectURI { get; set; }
+        public string ResponseType { get; set; }
+    }
+}
