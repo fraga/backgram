e4a98306d99d15339d3d3880be51c22911af031a
Rodrigo Fraga <rodrigo.fraga@gmail.com>
2013-07-31 07:59:17 -0400

    Adds OAuth20 component

--- Backgram.InstagramApi/OAuth20.cs
+++ Backgram.InstagramApi/OAuth20.cs
@@ -3,10 +3,50 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Backgram.Core.Api;
+using System.Net.Http;
 
 namespace Backgram.InstagramApi
 {
-    class OAuth20
+    public class OAuth20: IInstagramRestfulEndpoint
     {
+        public string ClientId { get; set; }
+        public string ClientSecret { get; set; }
+        public string RedirectUri { get; set; }
+        public string AccessToken { get; set; }
+        public string EndPoint
+        {
+            get { return "https://api.instagram.com/oauth/authorize/?client_id={0}&redirect_uri={1}&response_type=token"; }
+        }
+
+        public string Get()
+        {
+            using (HttpClient httpClient = new HttpClient())
+            {
+                var httpContent = httpClient.GetAsync(new Uri(String.Format(EndPoint, ClientId, RedirectUri))).Result;
+
+                return httpContent.Content.ReadAsStringAsync().Result;
+            }
+        }
+
+        public string Post()
+        {
+            throw new NotSupportedException();
+        }
+
+        public string Delete()
+        {
+            throw new NotSupportedException();
+        }
+
+        public string Put()
+        {
+            throw new NotSupportedException();
+        }
+
+        public HttpContent BuildExplicitAuthHeader()
+        {
+            return null;
+        }
     }
 }
