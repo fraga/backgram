e0b8b30bf67d2cb8affe01b9345243a145ccbba5
Rodrigo Fraga <rodrigo.fraga@gmail.com>
2013-07-31 23:09:26 -0400

    Putting tests in place for the hooks

--- Backgram.Tests/Core/InstagramTest.cs
+++ Backgram.Tests/Core/InstagramTest.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Backgram.Core.Api;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,41 @@ using System.Threading.Tasks;
 
 namespace Backgram.Tests.Core
 {
-    class InstagramTest
+    [TestClass]
+    public class InstagramTest
     {
+        [TestMethod]
+        public void AssertIsNotNull()
+        {
+            Instagram instagram = new Instagram();
+            Assert.IsNotNull(instagram);
+        }
+
+        [TestMethod]
+        public void TestCatalogShouldHookAssembly()
+        {
+            Instagram instagram = new Instagram();
+            instagram.ImportCatalog();
+
+            Assert.IsNotNull(instagram.InstagramAuthEndpoints);
+            Assert.IsNotNull(instagram.InstagramEndpoints);
+        }
+
+        [TestMethod]
+        public void TestCatalogShouldLoadClasses()
+        {
+            Instagram instagram = new Instagram();
+            instagram.ImportCatalog();
+
+            foreach (var item in instagram.InstagramEndpoints)
+            {
+                Assert.IsNotNull(item.Value);
+            }
+
+            foreach (var item in instagram.InstagramAuthEndpoints)
+            {
+                Assert.IsNotNull(item.Value);
+            }
+        }
     }
 }
