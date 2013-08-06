3a97ee30c0659cfdbd56aaa012cad98959b852e3
Rodrigo Fraga <rodrigo.fraga@gmail.com>
2013-08-05 22:31:54 -0400

    We don't need OAuthRedirect for now
    Renaming the OAuthRedirectTest to OAuthAuthorizeCode

--- Backgram.Tests/InstagramApi/OAuthRedirectTest.cs
+++ Backgram.Tests/InstagramApi/OAuthRedirectTest.cs
@@ -10,33 +10,13 @@ using System.Threading.Tasks;
 namespace Backgram.Tests.InstagramApi
 {
     [TestClass]
-    public class OAuthRedirectTest
+    public class OAuthAuthorizeCode
     {
         [TestMethod]
-        public void TestOAuthRedirectConstructor()
+        public void TestOAuthAuthorizeCodeConstructor()
         {
-            OAuthRedirect auth = new OAuthRedirect();
+            OAuthAuthorizeCode auth = new OAuthAuthorizeCode();
             Assert.IsNotNull(auth);
-        }
-
-        [TestMethod]
-        public void TestOAuthRedirectInstagramDataIsNotNull()
-        {
-            OAuthRedirect auth = new OAuthRedirect();
-            Assert.IsNotNull(auth);
-            Assert.IsNotNull(auth.InstagramData);
-        }
-
-        [TestMethod]
-        public void TestOAuthRedirectGet()
-        {
-            OAuthRedirect auth = new OAuthRedirect();
-            InstagramData data = new InstagramData { ClientId = "client_id", ClientSecret = "client_secret", RedirectURI = "redirect_uri", ResponseType = "token" };
-
-            auth.InstagramData = data;
-            var result = auth.Get();
-
-            Assert.IsFalse(string.IsNullOrEmpty(result));
         }
 
     }
