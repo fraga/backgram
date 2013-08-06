80acbf2930ad64949bcff771df86fbe0a363168a
Rodrigo Fraga <rodrigo.fraga@gmail.com>
2013-08-05 22:58:36 -0400

    Adds UsersSelfFeedEndpoint

---
+++ Backgram.InstagramApi/UsersSelfFeedEndpoint.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Backgram.InstagramApi
+{
+    public class UsersSelfFeedEndpoint: BaseInstagramEndpoint
+    {
+        public string Endpoint { get { return "https://api.instagram.com/v1/users/self/feed?access_token={0}"; } }
+
+        public override string Get()
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                var result = client.GetAsync(new Uri(String.Format(Endpoint, InstagramData.AccessToken))).Result;
+
+                return result.Content.ReadAsStringAsync().Result;
+            }
+
+        }
+    }
+}
